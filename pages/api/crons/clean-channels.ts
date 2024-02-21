5263a5d1abbddaf833b57374639bcc8cb84d7892
lianghanzhong <wyattliang@gmail.com>
2024-02-21 15:59:36 +0800

    feat: add clean channel cron

---
+++ pages/api/crons/clean-channels.ts
@@ -0,0 +1,38 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { StreamChat } from 'stream-chat';
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse,
+) {
+    if (req.headers['Authorization'] !== `Bearer ${process.env.CRON_SECRET}`) {
+        return res.status(401).end('Unauthorized');
+    }
+
+    const api_key = process.env.STEAMIO_KEY!;
+    const api_secret = process.env.STEAMIO_SECRET;
+    const serverClient = StreamChat.getInstance(api_key, api_secret);
+
+    const filter = { last_message_at: { $lte: new Date(Date.now() - 60 * 60 * 1000) } };
+
+    const channels = await serverClient.queryChannels(
+        filter,
+        undefined,
+        {
+            watch: false,
+            state: true,
+        },
+    );
+
+    const deleteCids: string[] = [];
+    for (const channel of channels) {
+        const result = await channel.query({});
+        if (!result.watcher_count) {
+            deleteCids.push(channel.cid);
+        }
+    }
+    console.info(`Delete channels: ${deleteCids}`);
+    await serverClient.deleteChannels(deleteCids, { hard_delete: true });
+
+    res.status(200).json({ message: 'success' });
+}
